b6ead8e7a186f0594bb4def5c2aab68447cbdef2
Amal S <AmalSajiv>
2023-01-05 10:38:08 +0530

    feat(style): Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
