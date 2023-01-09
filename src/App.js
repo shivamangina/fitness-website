641db0dc250490dab7e512211c032b68e2175f9f
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2023-01-09 19:12:23 +0530

    refactor: Refactor layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Classes from './components/Classes';
-import Homes from './pages/Homes';
-
+import React from 'react';
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
+import Home from './Layout/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Homes />} />
-        <Route path='/classes' element={<Classes/>} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/classes" element={<Classes />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Intro from '../components/Intro';
 import Main from '../components/Main';
 import Services from '../components/Services';
@@ -8,20 +6,15 @@ import About from '../components/About';
 import Testomonials from '../components/Testomonials';
 import Contact from '../components/Contact';
 
-function Homes() {
+export default function Home() {
   return (
     <div>
-      <Header />
-
       <Main />
       <Services />
       <Intro />
       <About />
       <Testomonials />
       <Contact />
-      <Footer />
     </div>
   );
 }
-
-export default Homes;
