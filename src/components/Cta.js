a902564522d0f6049258de7dadd05753894dbf07
Amal S <AmalSajiv>
2023-01-12 14:29:43 +0530

    feat: Add contact

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -30,7 +30,7 @@ export default class Contact extends Component {
     //     console.log(data);
     //   });
     
-    console.log("Sucessfully sbmited the email: "+ email);
+    console.log("Sucessfully submitted the email: "+ email);
 
 
     this.setState({ email: '' });
